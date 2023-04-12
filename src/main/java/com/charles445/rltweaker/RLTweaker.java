bd4872bb627c55d14993a1e3feb6bf813977cebc
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-04-12 02:21:58 +0200

    Version 1.12.2-0.5.12

--- src/main/java/com/charles445/rltweaker/RLTweaker.java
+++ src/main/java/com/charles445/rltweaker/RLTweaker.java
@@ -88,7 +88,7 @@ public class RLTweaker
 {
 	public static final String MODID = "rltweaker";
 	public static final String NAME = "RLTweaker";
-	public static final String VERSION = "0.5.11";
+	public static final String VERSION = "0.5.12";
 	public static final VersionDelimiter VERSION_DELIMITER = VersionDelimiter.parse(VERSION);
 	
 	@Mod.Instance(RLTweaker.MODID)
