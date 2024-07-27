ad6fa8cc684239f88c82af389f0222baaf500ec5
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-07-27 11:23:14 +0200

    Remove advisor command

--- src/main/java/com/charles445/rltweaker/RLTweaker.java
+++ src/main/java/com/charles445/rltweaker/RLTweaker.java
@@ -11,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 import com.charles445.rltweaker.capability.ITweakerCapability;
 import com.charles445.rltweaker.capability.TweakerCapability;
 import com.charles445.rltweaker.capability.TweakerStorage;
-import com.charles445.rltweaker.command.CommandAdvisor;
 import com.charles445.rltweaker.command.CommandErrorReport;
 import com.charles445.rltweaker.command.CommandRLTweakerConfig;
 import com.charles445.rltweaker.command.CommandReloadInvestigateAIConfig;
@@ -301,7 +300,6 @@ public class RLTweaker
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent event)
 	{
-		event.registerServerCommand(new CommandAdvisor());
 		event.registerServerCommand(new CommandErrorReport());
 		event.registerServerCommand(new CommandRLTweakerConfig());
 		event.registerServerCommand(new CommandReloadInvestigateAIConfig());
